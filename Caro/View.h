fb736202b18984c5c29a48dc2f9b9d6857522220
Hoang Vu <24120493@student.hcmus.edu.vn>
2026-04-02 21:38:46 +0700

    Add project files.

---
+++ Caro/View.h
@@ -0,0 +1,14 @@
+﻿#ifndef VIEW_H
+#define VIEW_H
+void DrawBoardRaylib();
+
+// Hàm quản lý Màn hình Menu
+void DrawAndHandleMenu(Texture2D background, Font gameFont);
+
+void HandleGameInput();
+void DrawGameUI(Texture2D background, Font gameFont);
+void DrawAndHandleGameOver(Font gameFont);
+void DrawAndHandlePauseMenu(Font gameFont);
+
+void DrawAndHandleGame(Texture2D background, Font gameFont);
+#endif
---
+++ Caro/main.cpp
@@ -0,0 +1,52 @@
+﻿#include "raylib.h"
+#include "Data.h"
+#include "Logic.h"
+#include "View.h"
+#include "Bot.h"
+
+// Định nghĩa biến toàn cục
+_POINT _A[BOARD_SIZE][BOARD_SIZE];
+bool _TURN;
+int _COMMAND;
+int _X, _Y;
+WinLine _winLine;
+int _p1Moves = 0;
+int _p2Moves = 0;
+int _WINNER = 2;
+int _GAME_STATE = 0;
+int _GAME_MODE = 1;
+bool _IS_PAUSED = false;
+
+//hàm main
+int main() {
+    InitWindow(1450 , 900 , "Do An Caro - KHTN - MENU & BOT");
+    SetTargetFPS(60);
+    SetExitKey(0);
+    Texture2D background = LoadTexture("background.png");
+    Texture2D menuBg = LoadTexture("menu_bg.png");
+    Font gameFont = LoadFont("game_font.ttf");
+
+    ResetData();
+    _p1Moves = 0; _p2Moves = 0; _WINNER = 2;
+
+    int testLeft = 300;     // Tọa độ X
+    int testTop = 150;      // Tọa độ Y
+    int testCellSize = 30;  // Độ rộng 1 ô cờ
+
+    while (!WindowShouldClose()) {
+
+        
+        if (_GAME_STATE == 0) {
+            DrawAndHandleMenu(menuBg, gameFont);
+        }
+        else if (_GAME_STATE == 1) {
+            DrawAndHandleGame(background, gameFont);
+        }
+
+    }
+
+    UnloadFont(gameFont);
+    UnloadTexture(menuBg);
+    CloseWindow();
+    return 0;
+}
